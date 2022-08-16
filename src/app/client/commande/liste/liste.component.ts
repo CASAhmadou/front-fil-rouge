bf6d652c79bf72474aa49aa96a8fa6002931c11f
CASAhmadou <amalasakho92@gmail.com>
2022-08-16 21:33:13 +0000

    i

--- src/app/client/commande/liste/liste.component.ts
+++ src/app/client/commande/liste/liste.component.ts
@@ -1,4 +1,16 @@
+
+export interface Commande {
+  id?:number
+  numeroCommande?:string
+  dateCommande?:Date
+  etat?:string
+  montantCommande?:string
+}
+
+
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CommandeService } from '../../shared/service/commande.service';
 
 @Component({
   selector: 'cas-liste',
@@ -7,10 +19,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListeComponent implements OnInit {
 
-  constructor() { }
+  commandes$: Observable<Commande> | any = null
+  lenghTotal: number = 0
+  i:number=1
+  commandes: any[]=[]
+
+  constructor(private comService : CommandeService) { }
 
   ngOnInit(): void {
-    // alert('ok')
+    this.comService.commandeClient().subscribe(data=>{
+      console.log(data)
+      this.commandes = data
+      this.lenghTotal = this.commandes.length
+      return data
+    })
   }
+
+
 
 }
